583a567746edcc330c8ba7a3f06113818f9f1070
faridguluzade <tu5mhbxzt.code.edu.az>
2023-06-10 11:37:08 -0800

    project was build

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,18 +1,20 @@
-import { useState, useEffect, Fragment } from 'react';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect, Fragment } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
-import ProductCard from '../../components/product-card/product-card.component';
+import ProductCard from "../../components/product-card/product-card.component";
 
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesMap } from "../../store/categories/category.selector";
 
-import { CategoryContainer, Title } from './category.styles';
+import { CategoryContainer, Title } from "./category.styles";
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+
   const [products, setProducts] = useState(categoriesMap[category]);
 
+  console.log(categoriesMap);
   useEffect(() => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
